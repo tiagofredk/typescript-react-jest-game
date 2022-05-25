671acb67f99841d074435ccd0ad5cacec40dbebb
tiagofredk <tiagofredk@gmail.com>
2022-05-25 17:12:02 +0200

    ok

--- src/components/ingame/Board.tsx
+++ src/components/ingame/Board.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 export default function Board() {
 
-    const myRef = React.useRef<HTMLDivElement>(null);
-
-    const runCallback = () => {
+    const createPlaces = () => {
         const row = [];
         for (var i = 1; i <= 99; i++) {
             row.push(
-                <div ref={myRef} key={i} className={`place${i}`}>
+                <div key={i} className={`place${i}`}>
                     <p>{i}</p>
                 </div>
             );
@@ -16,25 +14,11 @@ export default function Board() {
         return row;
     }
 
-    function getPosition() {
-        const element = document.getElementsByClassName('place3')[0];
-        const rect = element.getBoundingClientRect();
-        
-        console.log(element);
-        console.log(rect.y);
-        console.log(rect.x);
-        // console.log(rect);
-        // console.log(`Coordinates: X: ${x}px, Y: ${y}px`);
-        // return { x, y };
-    }
-    // document.getElementsByClassName('place1')[0].addEventListener("click", getPosition);
-
     return (
         <div className='board-container'>
             <section className='board'>
-                {runCallback()}
+                {createPlaces()}
             </section>
-            <button onClick={()=> getPosition()}>Position</button>
         </div>
     )
 }
